ab2e39e0da1755a10fb85ae0b23b2eae8548abe6
Alvin Deng <alvin.q.deng@utexas.edu>
2017-08-09 20:50:46 -0700

    Update: Added some documentation to tf

--- tensorflow/mnist.py
+++ tensorflow/mnist.py
@@ -1,23 +1,27 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+# Downloading MNIST data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
-# Standard softmax
+# Standard soft max
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
+# Placeholders for storing the labels
 y_ = tf.placeholder(tf.float32, [None, 10])
+# Getting the cross entropy loss
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+# Initializing optimizer and throwing the loss function into the optimizer
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 session = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-# Training
+# Training standard soft max
 for _ in range(100):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     session.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
@@ -27,7 +31,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
-# Adding convolution
+# Initialization methods for weights
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -38,15 +42,18 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
+# Convolution 2D operation
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
+# Max pool 2x2 operation
 def max_pool_2x2_(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
+# CNN architecture
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
@@ -70,13 +77,18 @@ h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
+# Matmul for fast matrix multiplication
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
+# Reduce methods to calculate cross entropy loss
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+
+# test accuracy = 0.9921000003814697
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
     for iteration in range(20000):
@@ -92,6 +104,7 @@ with tf.Session() as session:
             )
             print("Step {}, training accuracy = {}".format(iteration, train_accuracy))
 
+        # Whenever you are running the graph, make sure to fill in placeholders with feed_dict
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
     print("test accuracy = {}".format(accuracy.eval(
